1c2579a8584f489bb4ec91c7491d09b2f28d137e
erdogan98 <35006163+erdogan98@users.noreply.github.com>
2019-03-12 11:17:19 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ReadWriteUsers
+{
+    partial class SAMS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.studentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.moduleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Module = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentspublicBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Writebtn = new System.Windows.Forms.Button();
+            this.Readbtn = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentspublicBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.studentIDDataGridViewTextBoxColumn,
+            this.StudentName,
+            this.emailDataGridViewTextBoxColumn,
+            this.moduleDataGridViewTextBoxColumn,
+            this.Module,
+            this.dataGridViewTextBoxColumn1});
+            this.dataGridView.DataSource = this.studentspublicBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(803, 409);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // studentIDDataGridViewTextBoxColumn
+            // 
+            this.studentIDDataGridViewTextBoxColumn.DataPropertyName = "StudentID";
+            this.studentIDDataGridViewTextBoxColumn.HeaderText = "StudentID";
+            this.studentIDDataGridViewTextBoxColumn.Name = "studentIDDataGridViewTextBoxColumn";
+            // 
+            // StudentName
+            // 
+            this.StudentName.DataPropertyName = "StudentName";
+            this.StudentName.HeaderText = "StudentName";
+            this.StudentName.Name = "StudentName";
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            // 
+            // moduleDataGridViewTextBoxColumn
+            // 
+            this.moduleDataGridViewTextBoxColumn.DataPropertyName = "Module";
+            this.moduleDataGridViewTextBoxColumn.HeaderText = "Module";
+            this.moduleDataGridViewTextBoxColumn.Name = "moduleDataGridViewTextBoxColumn";
+            // 
+            // Module
+            // 
+            this.Module.DataPropertyName = "Module2";
+            this.Module.HeaderText = "Module2";
+            this.Module.Name = "Module";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Module3";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Module3";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // studentspublicBindingSource
+            // 
+            this.studentspublicBindingSource.DataSource = typeof(ReadWriteUsers.Studentspublic);
+            // 
+            // Writebtn
+            // 
+            this.Writebtn.Location = new System.Drawing.Point(612, 415);
+            this.Writebtn.Name = "Writebtn";
+            this.Writebtn.Size = new System.Drawing.Size(75, 23);
+            this.Writebtn.TabIndex = 1;
+            this.Writebtn.Text = "Write";
+            this.Writebtn.UseVisualStyleBackColor = true;
+            this.Writebtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Readbtn
+            // 
+            this.Readbtn.Location = new System.Drawing.Point(693, 415);
+            this.Readbtn.Name = "Readbtn";
+            this.Readbtn.Size = new System.Drawing.Size(75, 23);
+            this.Readbtn.TabIndex = 2;
+            this.Readbtn.Text = "Read";
+            this.Readbtn.UseVisualStyleBackColor = true;
+            this.Readbtn.Click += new System.EventHandler(this.Readbtn_Click);
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(531, 415);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 3;
+            this.Button1.Text = "Swipe Card";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // SAMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Readbtn);
+            this.Controls.Add(this.Writebtn);
+            this.Controls.Add(this.dataGridView);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "SAMS";
+            this.Text = "Student Attendance Monitoring System";
+            this.TransparencyKey = System.Drawing.Color.Olive;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentspublicBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button Writebtn;
+        private System.Windows.Forms.Button Readbtn;
+        private System.Windows.Forms.BindingSource studentspublicBindingSource;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn moduleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Module;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+    }
+}
+
